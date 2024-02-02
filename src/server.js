b2ba9d84433e2727f12f5b5a7dea15693155a39b
miguellimasossego <miguel.tavares@sossego.com.br>
2024-02-01 21:05:53 -0300

    fix: server.js configurations

--- src/server.js
+++ src/server.js
@@ -1,7 +1,13 @@
+const express = require('express');
 const http = require('http');
-
 const getDamageSummaryByDateRoute = require('./get-damage-summary-by-date/route');
 
-const server = http.Server();
+const app = express();
+// Middleware para interpretar JSON
+app.use(express.json());
+
+app[getDamageSummaryByDateRoute.method](getDamageSummaryByDateRoute.path, getDamageSummaryByDateRoute.fn);
+
+const server = http.createServer(app);
 
 module.exports = server;
