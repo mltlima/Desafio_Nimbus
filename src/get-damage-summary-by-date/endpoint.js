6a0666915786426c5feb243bc780a443e29fd7e8
miguellimasossego <miguel.tavares@sossego.com.br>
2024-02-01 21:00:59 -0300

    feat: configuracao endpoint

--- src/get-damage-summary-by-date/endpoint.js
+++ src/get-damage-summary-by-date/endpoint.js
@@ -1,7 +1,24 @@
 const controller = require('./controller');
 
+const execute = async (req, res) => {
+    try {
+        const { dateStart, dateEnd } = req.query;
+        
+        const damageSummaries = await controller.execute(dateStart, dateEnd);
+
+        // Se tudo estiver correto, envia a resposta com os sumários de danos
+        res.json({ data: damageSummaries });
+    } catch (error) {
+        // Se um erro for lançado, verifica o tipo e envia a resposta HTTP apropriada
+        if (error.message.includes('required')) {
+            res.status(400).json({ message: error.message });
+        } else {
+            // Para qualquer outro erro, envia um erro de servidor
+            res.status(500).json({ message: 'Internal server error' });
+        }
+    }
+};
+
 module.exports = {
-    execute() {
-        // TODO: tratamento da requisição e resposta do caso de uso
-    },
+    execute,
 };
